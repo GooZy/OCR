75fb6ae88817f199905ca68dcf7031fb8dc5db95
郭子尧 <guoziyao@bytedance.com>
2017-12-21 21:00:24 +0800

    feat: 放大

--- main.py
+++ main.py
@@ -4,15 +4,23 @@
 # @Author  : Guo Ziyao
 import cv2
 
+from util import correct_angle
 from util import show_img
 
 
 def img_cr(img_path):
     img = cv2.imread(img_path)
+    # 放大
+    h, w = img.shape[: 2]
+    img = cv2.resize(img, None, fx=256.0/w, fy=256.0/w, interpolation=cv2.INTER_CUBIC)
+    # 灰度
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # show_img(gray_img)
-    ret, binary_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU)
-    show_img(binary_img)
+    # 二值
+    ret, binary_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    # show_img(binary_img)
+    # 倾斜矫正
+    img = correct_angle(binary_img)
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -3,8 +3,30 @@
 # @Time    : 2017/12/20 09:18
 # @Author  : Guo Ziyao
 import cv2
+import numpy as np
 
 
 def show_img(img):
+    """
+    显示图像，按任意键退出
+    """
     cv2.imshow('test', img)
     cv2.waitKey(0)
+
+
+def correct_angle(img):
+    """
+    图像倾斜矫正
+    :return: 返回图像对象
+    """
+    return img
+
+
+if __name__ == '__main__':
+    img = cv2.imread('test/im0.png')
+    h, w = img.shape[: 2]
+    img = cv2.resize(img, None, fx=256.0 / w, fy=256.0 / w, interpolation=cv2.INTER_CUBIC)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, binary_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+
+    show_img(correct_angle(binary_img))
