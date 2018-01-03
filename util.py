0c9ffca87039ca9a7bfdb7a1eeedafc5e8152f75
郭子尧 <guoziyao@bytedance.com>
2018-01-03 20:41:27 +0800

    feat: 倾斜矫正

--- util.py
+++ util.py
@@ -16,9 +16,42 @@ def show_img(img):
 
 def correct_angle(img):
     """
-    图像倾斜矫正
+    图像倾斜矫正。找到外接矩形，然后取两条宽的中点算斜率，求倾斜角度
+    :img: 二值化图像
     :return: 返回图像对象
     """
+    blur = cv2.GaussianBlur(img, (51, 51), 0)
+    ret, binary_img = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    edges = cv2.Canny(binary_img, 100, 200)
+    image, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    rect = cv2.minAreaRect(contours[0])
+    # angle = rect[2]
+    # print angle
+    points = cv2.boxPoints(rect)
+    up, down, left, right = (1000, 0), (-1000, 0), (0, 1000), (0, -1000)
+    for each in points:
+        y, x = each
+        if y <= up[0]:
+            up = (y, x)
+        if y >= down[0]:
+            down = (y, x)
+        if x <= left[1]:
+            left = (y, x)
+        if x >= right[1]:
+            right = (y, x)
+    # print up, down, left, right
+    if up[1] > down[1]:
+        x1, y1 = (up[0] + right[0]) / 2.0, (up[1] + right[1]) / 2.0
+        x2, y2 = (down[0] + left[0]) / 2.0, (down[1] + left[1]) / 2.0
+        angle = np.arctan((y1 - y2) / (x1 - x2)) * 180 / np.pi
+    else:
+        x1, y1 = (up[0] + left[0]) / 2.0, (up[1] + left[1]) / 2.0
+        x2, y2 = (down[0] + right[0]) / 2.0, (down[1] + right[1]) / 2.0
+        angle = np.arctan((y1 - y2) / (x1 - x2)) * 180 / np.pi
+    rows, cols = img.shape[: 2]
+    M = cv2.getRotationMatrix2D((int(rows / 2), int(cols / 2)), angle, 1)
+    img = cv2.warpAffine(img, M, (rows, cols), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    # cv2.drawContours(img, contours, 0, (0, 0, 255), 1)
     return img
 
 
